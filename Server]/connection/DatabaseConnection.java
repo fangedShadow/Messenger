eaac96af78156dee2098d654d7f5e08780deeb19
fangedShadow <75034643+fangedShadow@users.noreply.github.com>
2023-12-06 21:43:12 -0500

    Add files via upload

---
+++ Server]/connection/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package com.raven.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    private DatabaseConnection() {
+
+    }
+
+    public void connectToDatabase() throws SQLException {
+       
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/messenger?serverTimezone=EST", "root", "BP18singh$02");
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
